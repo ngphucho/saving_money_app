257b0ceab751e9a3486e4c78766fe55e23543d42
Nguyễn Phúc Hồ <68779126+ngphucho@users.noreply.github.com>
2025-04-28 07:54:26 +0000

    cap nhat loading

--- src/components/LoadingComponent.jsx
+++ src/components/LoadingComponent.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Lottie from "react-lottie";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // **Quan trọng:** Thay thế đường dẫn này bằng URL trực tiếp của file JSON Lottie
 // hoặc import file JSON nếu bạn đã tải nó lên CodeSandbox.
 // Ví dụ (URL):
-import animationData from "../assets/heart_2s.json";
+import animationData from "../assets/loading2.json";
 
 // Ví dụ (import - giả sử bạn đã tạo file animation.json trong thư mục src):
 // import animationData from './animation.json';
@@ -29,13 +30,13 @@ function App() {
         flexDirection: "column",
       }}
     >
-      <h1>Lottie trong CodeSandbox</h1>
       {animationData && (
         <Lottie options={defaultOptions} height={200} width={200} />
       )}
       {!animationData && (
         <p>Vui lòng thay thế URL hoặc import file JSON Lottie.</p>
       )}
+      <h3 className="text-danger">... LOADING ...</h3>
     </div>
   );
 }
